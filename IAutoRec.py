eca0eea91bae9725e4a9e8cfd383549080c49fbd
kelvixzhang(张志恒) <kelvixzhang@tencent.com>
2017-02-24 19:04:59 +0800

    Add i auto rec

--- IAutoRec.py
+++ IAutoRec.py
@@ -27,64 +27,63 @@ except:
 k = 500
 epochCount = 300
 
-# load data TrainSet->[(uid, iid, rate)]
+# load data
 import data
 userCount, itemCount, trainSet, testSet = data.ml_1m(should_shuffle=False)
 
 # train data
-trainData = defaultdict(lambda:[0]*userCount) #Mat #Item * #User
-trainMask = defaultdict(lambda:[0]*userCount) #if Mat in item is exist
+trainData = defaultdict(lambda:[0]*itemCount)
+trainMask = defaultdict(lambda:[0]*itemCount)
 for t in trainSet:
     userId = int(t[0])
     itemId = int(t[1])
     rating = float(t[2])
-    trainData[itemId][userId] = rating
-    trainMask[itemId][userId] = 1.0
+    trainData[userId][itemId] = rating
+    trainMask[userId][itemId] = 1.0
 
 # test data
-missCnt = 0 #  num of item exist in testSet but not in trainSet
-testData = defaultdict(lambda:[0]*userCount) #Mat #Item * #User
-testMask = defaultdict(lambda:[0]*userCount) #if Mat in item is exist
+missCnt = 0
+testData = defaultdict(lambda:[0]*itemCount)
+testMask = defaultdict(lambda:[0]*itemCount)
 for t in testSet:
     userId = int(t[0])
     itemId = int(t[1])
     rating = float(t[2])
-    if itemId in trainData:
-        testData[itemId][userId] = rating
-        testMask[itemId][userId] = 1.0
+    if userId in trainData:
+        testData[userId][itemId] = rating
+        testMask[userId][itemId] = 1.0
     else:
         missCnt += 1
 
 # evaluate data
-allData     = [] #user_vec s of All Item in train data
-allTestData = [] #user_vec s of All Item in test data
-allTestMask = [] #user_mask_vec s of All Item in test data
-for itemId in testData:
-    allData.append(trainData[itemId])
-    allTestData.append(testData[itemId])
-    allTestMask.append(testMask[itemId])
+allData     = []
+allTestData = []
+allTestMask = []
+for userId in testData:
+    allData.append(trainData[userId])
+    allTestData.append(testData[userId])
+    allTestMask.append(testMask[userId])
 allData     = np.array(allData)
 allTestData = np.array(allTestData)
 allTestMask = np.array(allTestMask)
 
 # auto encoder
-#input #item * #user U AutoRec
-data = tf.placeholder(tf.float32, [None, userCount])
-mask = tf.placeholder(tf.float32, [None, userCount])
+data = tf.placeholder(tf.float32, [None, itemCount])
+mask = tf.placeholder(tf.float32, [None, itemCount])
 
 import math
 scale = math.sqrt(6.0 / (userCount + k))
 
-W1 = tf.Variable(tf.random_uniform([userCount, k], -scale, scale))
+W1 = tf.Variable(tf.random_uniform([itemCount, k], -scale, scale))
 b1 = tf.Variable(tf.random_uniform([k], -scale, scale))
 mid = tf.nn.softmax(tf.matmul(data, W1) + b1)
 
-W2 = tf.Variable(tf.random_uniform([k, userCount], -scale, scale))
-b2 = tf.Variable(tf.random_uniform([userCount], -scale, scale))
+W2 = tf.Variable(tf.random_uniform([k, itemCount], -scale, scale))
+b2 = tf.Variable(tf.random_uniform([itemCount], -scale, scale))
 y = tf.matmul(mid, W2) + b2
 
-preData = tf.placeholder(tf.float32, [None, userCount])
-preMask = tf.placeholder(tf.float32, [None, userCount])
+preData = tf.placeholder(tf.float32, [None, itemCount])
+preMask = tf.placeholder(tf.float32, [None, itemCount])
 rmse = tf.sqrt(tf.reduce_sum(tf.square((y - preData)*preMask)) / tf.reduce_sum(preMask))
 
 # training
@@ -96,21 +95,21 @@ sess.run(tf.global_variables_initializer())
 loss = tf.reduce_mean(tf.reduce_sum(tf.square((y - data)*mask), 1, keep_dims=True))
 trainStep = tf.train.GradientDescentOptimizer(learnRate).minimize(loss)
 
-itemIdList = trainData.keys()
+userIdList = trainData.keys()
 for epoch in range(epochCount):
-    random.shuffle(itemIdList)
+    random.shuffle(userIdList)
     
     # train
-    for batchId in range( len(itemIdList) / batchSize ):
+    for batchId in range( len(userIdList) / batchSize ):
         start = batchId * batchSize
         end = start + batchSize
 
         batchData = []
         batchMask = []
         for i in range(start, end):
-            itemId = itemIdList[i]
-            batchData.append(trainData[itemId])
-            batchMask.append(trainMask[itemId])
+            userId = userIdList[i]
+            batchData.append(trainData[userId])
+            batchMask.append(trainMask[userId])
 
         batchData = np.array(batchData)
         batchMask = np.array(batchMask)
@@ -119,4 +118,3 @@ for epoch in range(epochCount):
     # predict
     result = rmse.eval(feed_dict={data:allData, preData:allTestData, preMask:allTestMask})
     print("epoch %d/%d\trmse: %.4f"%(epoch+1, epochCount, result))
-
--- run_iautorec.sh
+++ run_iautorec.sh
@@ -1,6 +1,7 @@
+#!/usr/bin/env bash
 for batchSize in 512 256 128 64 32; do
     for learnRate in 0.03 0.02 0.01; do
     	echo "Train auto rec with "${batchSize}", "${learnRate}
-        python autoRec.py ${batchSize} ${learnRate} 1 > log/auto_rec_${batchSize}_${learnRate}.log &
+        python IAutoRec.py ${batchSize} ${learnRate} 1 > log/i_auto_rec_${batchSize}_${learnRate}.log &
     done
 done
